105bf931d976badc3a2b8a1a258213958dc0d82e
J210172 <210172@home.casa>
2024-02-12 00:20:02 +0100

    Fixed styles, added case insensitive comparision to the switch, added options in alerts and messages;

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,45 +1,68 @@
-const add_alert = (container_selector, message, type, closable = false, prepend = true) => {
+/**
+ * Agrega una alerta en el selector aportado
+ * 
+ * @param {String} container_selector Selector donde se intdroducira el mensaje
+ * @param {String} titulo El titulo del mensaje
+ * @param {String} message Cuerpo del mensaje
+ * @param {Boolean} closable Determina si tendra boton para cerrar el mensaje
+ * @param {Boolean} prepend Determina si se introducira al inicio o al final
+ * @param {Boolean} centered Determina si el mensaje aparecera centrado
+ */
+const add_alert = (container_selector, message, type, {closable = false, prepend = true} = {}) => {
     const container = document.querySelector(container_selector);
     const alert_element = document.createElement('div');
     
-    let button = '';
-   
-    if (closable)
-        button = '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-
-    let temp_element = [
-    `<div class="alert alert-${type} ${ closable ? "" : "p-2" } alert-dismissible" role="alert">${button}`,
-    `   <div>${message}</div>`,
-    '</div>'
-    ];
-
+    if (!container)
+        throw new Error(`No se encuentra ningun elemento con el selector ${container_selector}`)    
     
-    alert_element.innerHTML = temp_element.join('');
+    alert_element.classList.add('alert', 'alert-'+type);
     
+    if (!closable)
+        alert_element.classList.add('p-2');
+
+    alert_element.innerHTML = [
+        `<div>${message}</div>`,
+        closable ? '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>' : ''
+    ].join('');
+
     if (prepend)
         container.prepend(alert_element);
-    else 
+    else
         container.append(alert_element);
 };
 
-const add_message = function(container_selector, message, closable = false, prepend = true) {
+/**
+ * Agrega una caja de mensaje en selector aportado
+ * 
+ * @param {String} container_selector Selector donde se intdroducira el mensaje
+ * @param {String} titulo El titulo del mensaje
+ * @param {String} message Cuerpo del mensaje
+ * @param {Boolean} closable Determina si tendra boton para cerrar el mensaje
+ * @param {Boolean} prepend Determina si se introducira al inicio o al final
+ * @param {Boolean} centered Determina si el mensaje aparecera centrado
+ */
+const add_message = (container_selector, titulo, message, {closable = false, prepend = true, centered = false} = {}) => {
     const container = document.querySelector(container_selector);
     const message_element = document.createElement('div');
     
-    let button = closable ? '<button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>' : '';
+    if (!container)
+        throw new Error(`No se encuentra ningun elemento con el selector ${container_selector}`)
     
+    message_element.classList.add('toast', 'show', 'mt-3');
+    
+    if (centered)
+        message_element.classList.add('mx-auto');
+
     message_element.innerHTML = [
-        '<div class="toast show mt-3" role="alert" aria-live="assertive" aria-atomic="true">',
-        '    <div class="toast-header">',
-        `       <strong class="me-auto">Resultado</strong>`,
-        `       ${button}`,
-        '    </div>',
-        `    <div class="toast-body">${message}</div>`,
-        '</div>'
+        '<div class="toast-header">',
+        `<strong class="me-auto">${titulo}</strong>`,
+        closable ? '<button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>' : '',
+        '</div>',
+        `<div class="toast-body">${message}</div>`,
     ].join('')
 
     if (prepend)
         container.prepend(message_element);
     else
         container.append(message_element);
-}
+};
--- ejer_switch/ejer.js
+++ ejer_switch/ejer.js
@@ -4,14 +4,15 @@ $(function() {
 });
 
 function toping_price_button() {
-    let toping = document.querySelector("#toping");
+    let toping_input = document.querySelector("#toping");
     let price = 1.9;
     let added_price = 0;
+    let topping = toping_input.value.toLowerCase();
     let message = `<p>El helado cuesta “inicialmente” ${ price }.</p>`;
 
     if (!result || !toping) throw new Error("No hay input#toping o/y div#result")
 
-    switch (toping.value) {
+    switch (topping) {
         case "oreo":
             added_price = 1.0;
             break;
@@ -29,10 +30,10 @@ function toping_price_button() {
     }
 
     if (added_price != 0) {
-        message += `<p>El toping '${toping.value}' cuesta '${ added_price }'</p>`;
+        message += `<p>El toping '${topping}' cuesta '${ added_price }'</p>`;
         message += `<p>El helado con el toping elejido cuesta '${ price + added_price }'</p>`;
     }
 
-    add_message('#result', message);
+    add_message('#result', 'Resultado', message, {centered: true});
 }
 
